f6b9124b0321ca2b6101b2ac623602a8e2727da6
John Bernard Luna <125585580+NimoLuna@users.noreply.github.com>
2024-10-08 08:38:13 +0000

    main

--- Tasksheet113.java
+++ Tasksheet113.java
@@ -1,6 +1,5 @@
 public class Tasksheet113 {
     public static void main(String[] args) {
-        // Declaring primitive types
         byte zero = 0;
         char h = 'H';
         int three = 3;
@@ -8,10 +7,8 @@ public class Tasksheet113 {
         float twoPointZero = 2.0f;
         boolean isTrue = true;
 
-        // Concatenating the values into a string
         String output = "" + h + three + one + one + zero + " w" + zero + "r" + one + "d " + twoPointZero + " " + isTrue;
 
-        // Printing the result
         System.out.println(output);
     }
 }
