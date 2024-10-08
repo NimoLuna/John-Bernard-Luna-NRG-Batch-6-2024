f3e3089be9bbfe532f684207caee9a76b223c25f
John Bernard Luna <125585580+NimoLuna@users.noreply.github.com>
2024-10-08 08:35:57 +0000

    main

--- Tasksheet113.java
+++ Tasksheet113.java
@@ -1,5 +1,5 @@
 public class Tasksheet113 {
-    public static void Tasksheet113(String[] args) {
+    public static void main(String[] args) {
         System.out.println("H" + 3 + 1 + 1 + 0 + " w" + 0 + "r" + 1 + "d 2.0 true");
     }
 }
