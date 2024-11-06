c7236e2682707a2f3fc0704623e6fa9acaaddd90
John Bernard Luna <125585580+NimoLuna@users.noreply.github.com>
2024-11-06 00:02:21 +0000

    main

---
+++ Tasksheet128/AbstractShape.java
@@ -0,0 +1,11 @@
+public abstract class AbstractShape implements Shape {
+    protected String color;
+    protected double length;
+    protected double width;
+
+    public AbstractShape(String color, double length, double width) {
+        this.color = color;
+        this.length = length;
+        this.width = width;
+    }
+}
